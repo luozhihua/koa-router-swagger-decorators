c39f4e3f67aa492446e594a8e5be397f00f951cf
Colin Luo <mail@luozhihua.com>
2020-04-11 14:16:27 +0800

    add first and last hooks for request decorators: 3.7.0

--- declarations/decorators.d.ts
+++ declarations/decorators.d.ts
@@ -8,9 +8,12 @@ export declare function prefix(basePath?: string): (target: any) => any;
 export interface DecoratorWrapperOptions {
     before?(ctx: Context, target: any, name: string): Promise<void>;
     after?(ctx: Context, target: any, name: string): Promise<any>;
+    beforeFirst?(ctx: Context, target: any, name: string): Promise<void>;
+    afterLast?(ctx: Context, target: any, name: string): Promise<any>;
     formatter?: ResponseFormatter;
-    excludes?: string[];
 }
-export declare function wrapperProperty(target: any, descriptor: PropertyDescriptor, options?: Pick<DecoratorWrapperOptions, 'after' | 'before' | 'formatter'>): any;
-export declare function wrapperAll(target: any, options: DecoratorWrapperOptions): void;
+export declare function wrapperProperty(target: any, descriptor: PropertyDescriptor, options?: DecoratorWrapperOptions): any;
+export declare function wrapperAll(target: any, options: DecoratorWrapperOptions & {
+    excludes?: string[];
+}): void;
 export declare function requests(method: AllowedMethods, pathStr: string, formatter?: ResponseFormatter | boolean): (target: any, name: string, descriptor: PropertyDescriptor) => PropertyDescriptor;
