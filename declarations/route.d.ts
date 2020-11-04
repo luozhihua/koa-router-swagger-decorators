60f9ff941e5e327e13fd1a91ef39f6b61957dc74
Colin Luo <mail@luozhihua.com>
2020-11-04 09:06:46 +0800

    V4.0.4, exit CI steps

--- declarations/route.d.ts
+++ declarations/route.d.ts
@@ -1,3 +1,3 @@
 import { AllowedMethods, ResponseFormatter } from "./utils";
 export declare function formatter(formatter: ResponseFormatter | boolean): any;
-export declare function route(method: AllowedMethods, pathStr: string): (target: any, name: string, descriptor: PropertyDescriptor) => any;
+export declare function route(method: AllowedMethods, pathStr: string, render?: boolean): (target: any, name: string, descriptor: PropertyDescriptor) => any;
