90e63f826e018ce2a0ad13ee5f32ab6736b058c4
Colin Luo <mail@luozhihua.com>
2020-12-29 10:48:57 +0800

    V5.0.7, export ajv as validator.

--- declarations/index.d.ts
+++ declarations/index.d.ts
@@ -3,3 +3,4 @@ export * from "./utils";
 export * from "./decorators";
 export * from "./route";
 export { prefix, createRouter as default, requests as request, } from "./decorators";
+export { ajv as validator, Schema as ValidatorSchema, Params, } from "./validator";
