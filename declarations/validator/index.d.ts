40c4dbb5e75edceacbe73abef8d6c46b585ff4f4
Colin Luo <mail@luozhihua.com>
2020-12-29 10:54:01 +0800

    V5.0.8, export ajv as validator.

--- declarations/validator/index.d.ts
+++ declarations/validator/index.d.ts
@@ -1,10 +1,10 @@
 import { Context } from "koa";
 import Ajv, { JSONSchemaType } from "ajv";
 export declare type Schema<T> = {
-    [K in keyof T]: JSONSchemaType<T[K], true>;
+    [K in keyof T]?: JSONSchemaType<T[K], true>;
 };
 export declare type Params<T> = {
-    [K in keyof T]: T[K];
+    [K in keyof T]?: T[K];
 };
 export declare const ajv: Ajv;
 export default function KoaParamsValidator(ctx: Context, target: any, name: string): Promise<any>;
